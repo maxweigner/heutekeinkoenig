4ee16dcf068d8d00e94977181fd44810076ba69d
xxsoca <dennis.haupt@gmail.com>
2023-05-22 15:31:21 +0200

    dddd

--- logik.pl
+++ logik.pl
@@ -1,1 +1,17 @@
 % hier kommt der ganze logik bums hin
+
+% globalen Turncounter
+
+player_turn(Player) :-
+
+    % calculate action points
+    calc_action_points(Player, Turncounter)
+
+
+    % further logic
+
+
+
+
+    % save remaining ap
+    save_action_points(Player)
