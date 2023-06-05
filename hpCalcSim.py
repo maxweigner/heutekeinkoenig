0505f3f1c96c6f1df8b64b08c7e70001aa9b8910
Maximilian Wagner <maximilian.wagner@smail.inf.h-brs.de>
2023-06-05 11:37:37 +0200

    Extension des Simulators pog

--- hpCalcSim.py
+++ hpCalcSim.py
@@ -11,6 +11,8 @@ APog = [2,3,4,5]
 HPmult = 2
 APmult = 2
 
+simRes = ""
+
 print("AP:HP:HPnew\n")
 
 for i in range(0,4):
@@ -18,24 +20,33 @@ for i in range(0,4):
 	AP = HPog
 	match i:
 		case 0:
-			print("## Calculating without Multipliers ##")
+			caseString = "## Calculating without Multipliers ##"
+			print(caseString)
+			simRes += caseString
 
 		case 1:
-			print("\n## Calculating with Attack Multipliers ##")
+			caseString = "\n## Calculating with Attack Multipliers ##"
+			print(caseString)
+			simRes += caseString
 			AP = [point * 2 for point in AP]
 
 		case 2:
-			print("\n## Calculating with Defense Multipliers ##")
+			caseString = "\n## Calculating with Defense Multipliers ##"
+			print(caseString)
+			simRes += caseString
 			HP = [point * 2 for point in HP]
 			
 
 		case 3:
-			print("\n## Calculating with Attack and Defense Multipliers ##")
+			caseString = "\n## Calculating with Attack and Defense Multipliers ##"
+			print(caseString)
+			simRes += caseString
 			AP = [point * 2 for point in AP]
 			HP = [point * 2 for point in HP]
 
-
-	print("-First Iteration-")
+	itString = "\n-First Iteration-\n"
+	print(itString)
+	simRes += itString
 
 	liste = [AP, HP]
 	combinations = [p for p in it.product(*liste)]
@@ -44,11 +55,21 @@ for i in range(0,4):
 
 	for comb in combinations:
 		for soln in pl.query("einheit_alive("+str(comb[0])+","+str(comb[1])+", HPnew)"):
-			print(str(comb[0]) + ":" + str(comb[1]) + ":" + str(soln["HPnew"]))
+			tmpStr = str(comb[0]) + ":" + str(comb[1]) + ":" + str(soln["HPnew"]) + "\n"
+			print(tmpStr)
+			simRes += tmpStr
 			newcombs.append((comb[0], soln["HPnew"]))
 
-	print("-Second Iteration-")
+	itString = "\n-Second Iteration-\n"
+	print(itString)
+	simRes += itString
 	
 	for comb in newcombs:
 		for soln in pl.query("einheit_alive("+str(comb[0])+","+str(comb[1])+", HPnew)"):
-			print(str(comb[0]) + ":" + str(comb[1]) + ":" + str(soln["HPnew"]))
+			tmpStr = str(comb[0]) + ":" + str(comb[1]) + ":" + str(soln["HPnew"]) + "\n"
+			print(tmpStr)
+			simRes += tmpStr
+
+
+with open("res.txt", "w") as w:
+	w.write(simRes)
